7e43a4580be52085c5992c6706398baaabb0f5ef
Bhrevelec <166656993+Bhrevelec@users.noreply.github.com>
2024-05-19 16:59:38 +0200

    added basic UI

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ getComputerChoice = () => {
     ? "paper"
     : "scissors";
 };
-
+/*
 getHumanChoice = () => {
   let humanChoice = prompt("Rock, paper, scissors, shoot!").toLowerCase();
   if (humanChoice.match(/^(rock|paper|scissors)$/)) {
@@ -19,11 +19,30 @@ getHumanChoice = () => {
     getHumanChoice();
   }
 };
+*/
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const resultContainer = document.querySelector("#resultContainer");
+const scoreContainer = document.querySelector("#scoreContainer");
+
+rockBtn.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+
+paperBtn.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
 
 playRound = (humanChoice, computerChoice) => {
   if (humanChoice === computerChoice) {
-    console.log(`Tie, you both played ${humanChoice}. Try again.`);
-    playRound(getHumanChoice(), getComputerChoice());
+    //console.log(`Tie, you both played ${humanChoice}. Try again.`);
+    resultContainer.textContent = `Tie, you both played ${humanChoice}. Try again.`;
+    //playRound(getHumanChoice(), getComputerChoice());
   } else {
     if (
       (humanChoice === "rock") & (computerChoice === "paper") ||
@@ -31,16 +50,27 @@ playRound = (humanChoice, computerChoice) => {
       (humanChoice === "scissors") & (computerChoice === "rock")
     ) {
       //computer wins
-      console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
+      //console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
+      resultContainer.textContent = `You lose, ${computerChoice} beats ${humanChoice}.`;
       computerScore += 1;
+      scoreContainer.textContent = `Player: ${humanScore}, Computer: ${computerScore}`;
     } else {
       //human wins
-      console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+      //console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+      resultContainer.textContent = `You win, ${humanChoice} beats ${computerChoice}`;
       humanScore += 1;
+      scoreContainer.textContent = `Player: ${humanScore}, Computer: ${computerScore}`;
     }
+  }
+  if (humanScore >= 5) {
+    scoreContainer.textContent = `You win the game with ${humanScore} to ${computerScore}.`;
+  }
+  if (computerScore >= 5) {
+    scoreContainer.textContent = `You lose the game with ${humanScore} to ${computerScore}.`;
   }
 };
 
+/*
 playGame = () => {
   for (let i = 1; i <= 5; i++) {
     console.log(`ROUND ${i}:`);
@@ -54,3 +84,4 @@ playGame = () => {
 };
 
 playGame();
+*/
